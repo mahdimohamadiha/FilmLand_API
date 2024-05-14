edc0c7fc7b6da0ebef9502777848edd1040158fc
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2024-05-14 22:02:46 +0330

    Update Connection string and connect new databse server

--- FilmLand.Database/Connection.cs
+++ FilmLand.Database/Connection.cs
@@ -5,7 +5,7 @@
         public static string FilmLand()
         {
             var lines = File.ReadAllLines("../FilmLand_API/Database.txt");
-            var connectionString = "Server="+ lines[0] +";Database="+ lines[1] +";TrustServerCertificate=True;Trusted_Connection=True;MultipleActiveResultSets=true;";
+            var connectionString = "data source=" + lines[0] + ";initial catalog=" + lines[1] + ";persist security info=True;user id=" + lines[2] + ";password=" + lines[3] + ";MultipleActiveResultSets=True;TrustServerCertificate=True;";
             return connectionString;
         }
     }
